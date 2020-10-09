18ddf63c047e1d919cff3208810912f18f66069a
Anomalouss <anomalouss247@gmail.com>
2020-10-09 23:11:44 +0300

    Done some things I already forgot about
    Test commit

--- X_Breach/Assets/Scripts/Combat.cs
+++ X_Breach/Assets/Scripts/Combat.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
-    public Transform point;
     public float attackRange;
     public LayerMask hostile;
     public int dmg;
-    public Animator animator;
+
+    Animator animator;
+    Transform point;
+
+    void Start()
+    {
+        point = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
 
         if (Input.GetMouseButtonDown(1))
         {
+            animator.SetBool("IsFighting", true);
             Melee();
-
         }
+
+        if(Input.GetMouseButtonUp(1))
+            animator.SetBool("IsFighting", false);
     }
     void Melee()
     {
-        animator.SetTrigger("attack");
         Collider2D[] entity_array = Physics2D.OverlapCircleAll(point.position, attackRange, hostile);
         foreach (Collider2D Enemy in entity_array)
         {
             Debug.Log("we hit " + Enemy);
 
-            Enemy.gameObject.GetComponent<Enemy>().b_entity.TakeDamage(dmg);
+            if(animator.GetBool("IsFighting"))
+                Enemy.gameObject.GetComponent<Enemy>().b_entity.TakeDamage(dmg);
+
             Debug.Log(Enemy.gameObject.GetComponent<Enemy>().b_entity.health);
 
             if (Enemy.gameObject.GetComponent<Enemy>().b_entity.health <= 0)
--- X_Breach/Assets/Scripts/ScManagement.cs
+++ X_Breach/Assets/Scripts/ScManagement.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 
 public class ScManagement : MonoBehaviour
 {
-    public AudioSource aSrc;
     public void GameOver()
     {
         Scene gameOverScene = SceneManager.GetSceneByName("GameOver");
