c35267ca03cb99f8ffb96874f4b7dc683463f010
Anomalouss <anomalouss247@gmail.com>
2020-10-10 20:17:55 +0300

    Added a main menu

---
+++ X_Breach/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject credits;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        if(credits.activeSelf)
+            credits.SetActive(false);
+        else credits.SetActive(true);
+    }
+}
